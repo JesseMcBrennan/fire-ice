651e52d4d157060bb5d58fca6e953ff1816a20b9
Jesse McFadden <jessemcbrennan@gmail.com>
2018-09-13 10:29:23 -0600

    Add data to redux store

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,8 +1,8 @@
 
-
 export const fetchHouseData = async () => {
   const url = 'http://localhost:3001/api/v1/houses'
   const response = await fetch(url)
   const result = await response.json()
   return result
 }
+
